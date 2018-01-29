fe67b21c8bf8a1eda10818e51bd0e70e89fcb7fa
Dilshod Miralimov <miralimov@gmail.com>
2018-01-29 19:02:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,14 +6,10 @@
 // }
 // theBeatlesPlay();
 
-// function theBeatlesPlay(musicians, instruments) {
-//   var array = [];
-//   for (var i = 0; i < musicians.length; i++) {
-//     array.push(musicians[i] + " plays " + instruments[i]);
-//   }
-//   return array;
-// }
-
-const theBeatlesPlay = (musicians, instruments) =>
-  musicians.map((musician, i) =>
-    `${musician} plays ${instruments[i]`);
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (var i = 0; i < musicians.length; i++) {
+    array.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return array;
+}
