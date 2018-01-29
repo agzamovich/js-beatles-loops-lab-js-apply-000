79e1662b3caca708f3f65cabd81a351955d1778a
Dilshod Miralimov <miralimov@gmail.com>
2018-01-29 18:26:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function theBeatlesPlay([Beatles, bsb, Agulero, Michael Jackson],[Guitar, Musical instruments, Piano, Fortopiano]){
-  var arr = [" "];
+  var arr = " ";
   for(var i = 0; i < arr.length; i++){
       console.log(arr[0] +  " plays" + arr[2]);
   }
