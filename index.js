579c576ebdca4c4e0090b09d002da5ab958276d8
Dilshod Miralimov <miralimov@gmail.com>
2018-01-29 18:31:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(["Beatles", "Agulero", "Michael" "Jackson"], ["Guitar", "Musical", "Piano", "Fortopiano"]){
   var arr = " ";
   for(var i = 0; i <= arr.length; i++){
-      console.log(arr[i]);
+      console.log(arr[0][i]);
   }
 }
 theBeatlesPlay();
