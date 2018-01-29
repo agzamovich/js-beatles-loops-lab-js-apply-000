b8090c30d2787908fcfb5a0616472dec5e64e5aa
Dilshod Miralimov <miralimov@gmail.com>
2018-01-29 19:06:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,10 +6,25 @@
 // }
 // theBeatlesPlay();
 
-function theBeatlesPlay(musicians, instruments) {
-  var array = [];
-  for (var i = 0; i < musicians.length; i++) {
-    array.push(musicians[i] + " plays " + instruments[i]);
-  }
-  return array;
-}
+// function theBeatlesPlay(musicians, instruments) {
+//   var array = [];
+//   for (var i = 0; i < musicians.length; i++) {
+//     array.push(musicians[i] + " plays " + instruments[i]);
+//   }
+//   return array;
+// }
+
+const theBeatlesPlay = (musicians, instruments) =>
+  musicians.map((musician, i) =>
+    `${musician} plays ${instruments[i]}`);
+
+theBeatlesPlay([
+  "Beatles",
+  "Agulero",
+  "Michael Jackson"
+], [
+  "Guitar",
+  "Musical",
+  "Piano",
+  "Fortopiano"
+]);
